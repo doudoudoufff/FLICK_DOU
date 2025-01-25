13b2d008b8887471b59be9b2558a5763f5c8ddf8
dou <danziyi9@gmail.com>
2025-01-25 23:00:42 +0800

    实现中文日历

---
+++ FLICK/Date+Extensions.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+extension Date {
+    func chineseStyleString() -> String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "zh_CN")
+        formatter.dateFormat = "yyyy年M月d日"
+        return formatter.string(from: self)
+    }
+    
+    func chineseStyleShortString() -> String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "zh_CN")
+        formatter.dateFormat = "M月d日"
+        return formatter.string(from: self)
+    }
+} 
